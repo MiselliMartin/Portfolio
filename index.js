4d87742f98e67ef65ebe6bc856c455e73ed8eef5
Martín Miselli <martin.miselli@gmail.com>
2023-06-12 23:55:37 -0300

    navbarsiguevista

--- index.js
+++ index.js
@@ -15,6 +15,44 @@ document.addEventListener("DOMContentLoaded", () => {
       navbar.style.position = "fixed";
       navbar.classList.toggle("aparecer");
     });
+
+    const secciones = document.querySelectorAll('.indicador')
+    const navlinks = document.querySelectorAll('.navlink')
+    const iconcitos = document.querySelectorAll('.fa-circle')
+
+    const funcionObserver = entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const itemActual = Array.from(navlinks).find(navlink => navlink.dataset.url === entry.target.id)
+          itemActual.classList.add('active')
+          for (const navlink of navlinks) {
+            if (itemActual != navlink){
+              if (navlink.classList.contains("active")){
+                navlink.classList.remove('active')
+              }
+            }
+          }
+          const iconitoActual = Array.from(iconcitos).find(iconito => iconito.dataset.url === entry.target.id)
+          iconitoActual.classList.add('fa-solid')
+          for (const iconito of iconcitos) {
+            if (iconitoActual != iconito){
+              if (iconito.classList.contains("fa-solid")){
+                iconito.classList.remove('fa-solid')
+                iconito.classList.add('fa-ragular')
+              }
+            }
+          }
+        }
+      })
+    }
+
+    const observer = new IntersectionObserver(funcionObserver, {
+      root: null,
+      rootMargin : '0px',
+      threshold: 0.8
+    })
+
+    secciones.forEach(seccion => observer.observe(seccion))
 });
   
   
